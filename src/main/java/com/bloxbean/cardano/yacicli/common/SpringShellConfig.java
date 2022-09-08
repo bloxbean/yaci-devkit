ec2d975c95077aaa152c4719ec07cd2ddd55ac9b
Satya <satran004@gmail.com>
2022-09-08 23:30:19 +0800

    Removed lazy initialization

--- src/main/java/com/bloxbean/cardano/yacicli/common/SpringShellConfig.java
+++ src/main/java/com/bloxbean/cardano/yacicli/common/SpringShellConfig.java
@@ -3,13 +3,12 @@ package com.bloxbean.cardano.yacicli.common;
 import org.jline.terminal.Terminal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class SpringShellConfig {
 
     @Bean
-    public ShellHelper shellHelper(@Lazy Terminal terminal) {
+    public ShellHelper shellHelper(Terminal terminal) {
         return new ShellHelper(terminal);
     }
 
