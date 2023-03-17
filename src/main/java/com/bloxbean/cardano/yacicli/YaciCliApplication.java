e648991ad0cd4e57ba9251a9f857a32af6c3fce5
Satya <satran004@gmail.com>
2023-03-17 12:19:53 +0800

    Add support for multiple comma separated commands during non interactive mode

--- src/main/java/com/bloxbean/cardano/yacicli/YaciCliApplication.java
+++ src/main/java/com/bloxbean/cardano/yacicli/YaciCliApplication.java
@@ -3,13 +3,20 @@ package com.bloxbean.cardano.yacicli;
 import com.bloxbean.cardano.yacicli.commands.localcluster.ClusterConfig;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.shell.jline.NonInteractiveShellRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @Slf4j
@@ -24,9 +31,11 @@ public class YaciCliApplication {
 	@Component
 	public class InitBean{
 		ClusterConfig clusterConfig;
+        NonInteractiveShellRunner nonInteractiveShellRunner;
 
-		public InitBean(ClusterConfig clusterConfig) {
+		public InitBean(ClusterConfig clusterConfig, NonInteractiveShellRunner nonInteractiveShellRunner) {
 			this.clusterConfig = clusterConfig;
+            this.nonInteractiveShellRunner = nonInteractiveShellRunner;
 		}
 
 		@PostConstruct
@@ -34,7 +43,25 @@ public class YaciCliApplication {
 			Path path = Path.of(clusterConfig.getCLIBinFolder());
 			if (!Files.exists(path))
 				Files.createDirectories(path);
+
+            nonInteractiveShellRunner.setCommandsFromInputArgs(commandsFromInputArgs);
 		}
 	}
+
+    //Used to parse multiple comma separate commands for NonInteractiveShellRunner
+    private Function<ApplicationArguments, List<String>> commandsFromInputArgs = args -> {
+        if (args.getSourceArgs().length == 0) {
+            return Collections.emptyList();
+        }
+
+        String raw = Arrays.stream(args.getSourceArgs())
+                .collect(Collectors.joining(" "));
+        String[] commands = raw.split(",");
+        for (String cmd: commands) {
+            if (log.isDebugEnabled())
+                log.debug("Command: " +  cmd);
+        }
+        return Arrays.asList(commands);
+    };
 
 }
