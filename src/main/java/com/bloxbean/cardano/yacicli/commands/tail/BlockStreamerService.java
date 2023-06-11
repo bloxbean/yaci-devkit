96a5d7089c79fd92825a56cb857fa3777475df87
Satya <satran004@gmail.com>
2023-06-11 20:43:39 +0800

    Replace BlockStream with BlockSync in tail command

--- src/main/java/com/bloxbean/cardano/yacicli/commands/tail/BlockStreamerService.java
+++ src/main/java/com/bloxbean/cardano/yacicli/commands/tail/BlockStreamerService.java
@@ -1,14 +1,12 @@
 package com.bloxbean.cardano.yacicli.commands.tail;
 
 import com.bloxbean.cardano.yaci.core.common.Constants;
-import com.bloxbean.cardano.yaci.core.model.Amount;
-import com.bloxbean.cardano.yaci.core.model.TransactionBody;
-import com.bloxbean.cardano.yaci.core.model.TransactionInput;
-import com.bloxbean.cardano.yaci.core.model.TransactionOutput;
+import com.bloxbean.cardano.yaci.core.model.*;
 import com.bloxbean.cardano.yaci.core.protocol.chainsync.messages.Point;
 import com.bloxbean.cardano.yaci.core.protocol.handshake.messages.VersionTable;
 import com.bloxbean.cardano.yaci.core.protocol.handshake.util.N2NVersionTableConstant;
-import com.bloxbean.cardano.yaci.helper.reactive.BlockStreamer;
+import com.bloxbean.cardano.yaci.helper.BlockSync;
+import com.bloxbean.cardano.yaci.helper.listener.BlockChainDataListener;
 import com.bloxbean.cardano.yacicli.commands.tail.model.*;
 import com.bloxbean.cardano.yacicli.common.ConsoleHelper;
 import com.bloxbean.cardano.yacicli.output.OutputFormatter;
@@ -17,8 +15,6 @@ import com.bloxbean.cardano.yacicli.rule.RuleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import reactor.core.Disposable;
-import reactor.core.publisher.Flux;
 
 import java.math.BigInteger;
 import java.util.Collection;
@@ -82,120 +78,119 @@ public class BlockStreamerService {
 
         Result ruleResult = new Result();
 
-        Flux<List<TransactionBody>> stream = BlockStreamer.fromLatest(connection.getHost(), connection.getPort(), connection.getWellKnownPoint(), versionTable)
-                .stream()
-                .map(block -> {
-                    //Initialize ruleResult
-                    ruleResult.clear();
-                    ruleService.executeRules(block, ruleResult);
+        BlockSync blockSync = new BlockSync(connection.getHost(), connection.getPort(), connection.getProtocolMagic(), connection.getWellKnownPoint());
+        blockSync.startSyncFromTip(new BlockChainDataListener() {
+            @Override
+            public void onBlock(Block block) {
+                //Initialize ruleResult
+                ruleResult.clear();
+                ruleService.executeRules(block, ruleResult);
 
-                    outputBlock.setBlockNumber(block.getHeader().getHeaderBody().getBlockNumber());
-                    outputBlock.setBlockSize(block.getHeader().getHeaderBody().getBlockBodySize());
+                outputBlock.setBlockNumber(block.getHeader().getHeaderBody().getBlockNumber());
+                outputBlock.setBlockSize(block.getHeader().getHeaderBody().getBlockBodySize());
 
-                    List<String> metadataList = block.getAuxiliaryDataMap().values().stream()
-                            .map(auxData -> auxData.getMetadataJson())
-                            .collect(Collectors.toList());
-                    outputBlock.setMetadataList(metadataList);
+                List<String> metadataList = block.getAuxiliaryDataMap().values().stream()
+                        .map(auxData -> auxData.getMetadataJson())
+                        .collect(Collectors.toList());
+                outputBlock.setMetadataList(metadataList);
 
-                    long nV1scripts = block.getTransactionWitness()
-                            .stream().map(witnesses -> witnesses.getPlutusV1Scripts())
-                            .filter(plutusScripts -> plutusScripts != null)
-                            .flatMap(plutusScripts -> plutusScripts.stream())
-                            .collect(Collectors.toList())
-                            .size();
+                long nV1scripts = block.getTransactionWitness()
+                        .stream().map(witnesses -> witnesses.getPlutusV1Scripts())
+                        .filter(plutusScripts -> plutusScripts != null)
+                        .flatMap(plutusScripts -> plutusScripts.stream())
+                        .collect(Collectors.toList())
+                        .size();
 
-                    long nV2scripts = block.getTransactionWitness()
-                            .stream().map(witnesses -> witnesses.getPlutusV2Scripts())
-                            .filter(plutusScripts -> plutusScripts != null)
-                            .flatMap(plutusScripts -> plutusScripts.stream())
-                            .collect(Collectors.toList())
-                            .size();
+                long nV2scripts = block.getTransactionWitness()
+                        .stream().map(witnesses -> witnesses.getPlutusV2Scripts())
+                        .filter(plutusScripts -> plutusScripts != null)
+                        .flatMap(plutusScripts -> plutusScripts.stream())
+                        .collect(Collectors.toList())
+                        .size();
 
-                    outputBlock.setNoPlutusV1Scripts(nV1scripts);
-                    outputBlock.setNoPlutusV2Scripts(nV2scripts);
+                outputBlock.setNoPlutusV1Scripts(nV1scripts);
+                outputBlock.setNoPlutusV2Scripts(nV2scripts);
 
-                    return block.getTransactionBodies();
-                });
+                List<TransactionBody> transactionBodies = block.getTransactionBodies();
 
-        Disposable disposable = stream.subscribe(transactionBodies -> {
-            if (!grouping) {
-                transactionBodies.stream().forEach(transactionBody -> {
-                    if (showInputs) {
-                        // outputBlock.setShowInput(true);
-                        setInputs(transactionBody.getInputs(), outputBlock);
+                if (!grouping) {
+                    transactionBodies.stream().forEach(transactionBody -> {
+                        if (showInputs) {
+                            // outputBlock.setShowInput(true);
+                            setInputs(transactionBody.getInputs(), outputBlock);
+                        }
+
+                        if (showMint) {
+                            //outputBlock.setShowMint(true);
+                            setMint(transactionBody.getMint(), outputBlock);
+                        }
+
+                        transactionBody.getOutputs().forEach(transactionOutput -> setOutput(transactionOutput, outputBlock));
+
+                    });
+                    String formattedBlock = outputFormatter.formatBlock(outputBlock);
+                    System.out.println();
+                    System.out.println(formattedBlock);
+                    outputBlock.getInputs().clear();
+
+                } else {
+                    if (transactionBodies != null)
+                        outputBlock.setTotalTxs(transactionBodies.size());
+
+                    AtomicLong totalFees = new AtomicLong(0);
+                    transactionBodies.forEach(transactionBody -> totalFees.addAndGet(transactionBody.getFee().longValue()));
+                    outputBlock.setTotalFees(totalFees.longValue());
+
+                    for (TransactionBody body : transactionBodies) {
+                        List<String> inlineDatum = body.getOutputs().stream()
+                                .map(transactionOutput -> transactionOutput.getInlineDatum())
+                                .filter(iDatum -> iDatum != null)
+                                .collect(Collectors.toList());
+                        outputBlock.setInlineDatums(inlineDatum);
+
+                        List<String> datumHash = body.getOutputs().stream().map(transactionOutput -> transactionOutput.getDatumHash())
+                                .filter(dHash -> dHash != null)
+                                .collect(Collectors.toList());
+                        outputBlock.setDatumHashes(datumHash);
                     }
+
+                    outputBlock.setShowGrouping(true);
+                    List<Amount> amounts = transactionBodies.stream()
+                            .flatMap(transactionBody ->
+                                    transactionBody.getOutputs().stream()
+                                            .flatMap(txOutput -> txOutput.getAmounts().stream()))
+                            .collect(Collectors.toList());
+
+                    List<Amount> mintAmounts = transactionBodies.stream()
+                            .flatMap(transactionBody -> transactionBody.getMint().stream())
+                            .collect(Collectors.toList());
+
+                    setGroupAmount(amounts, outputBlock);
 
                     if (showMint) {
                         //outputBlock.setShowMint(true);
-                        setMint(transactionBody.getMint(), outputBlock);
+                        setMint(mintAmounts, outputBlock);
                     }
 
-                    transactionBody.getOutputs().forEach(transactionOutput -> setOutput(transactionOutput, outputBlock));
-
-                });
-                String formattedBlock = outputFormatter.formatBlock(outputBlock);
-                System.out.println();
-                System.out.println(formattedBlock);
-                outputBlock.getInputs().clear();
-
-            } else {
-                if (transactionBodies != null)
-                    outputBlock.setTotalTxs(transactionBodies.size());
-
-                AtomicLong totalFees = new AtomicLong(0);
-                transactionBodies.forEach(transactionBody -> totalFees.addAndGet(transactionBody.getFee().longValue()));
-                outputBlock.setTotalFees(totalFees.longValue());
-
-                for (TransactionBody body : transactionBodies) {
-                    List<String> inlineDatum = body.getOutputs().stream()
-                            .map(transactionOutput -> transactionOutput.getInlineDatum())
-                            .filter(iDatum -> iDatum != null)
+                    //Print Inputs
+                    List<TransactionInput> inputs = transactionBodies.stream()
+                            .flatMap(transactionBody -> transactionBody.getInputs().stream())
                             .collect(Collectors.toList());
-                    outputBlock.setInlineDatums(inlineDatum);
 
-                    List<String> datumHash = body.getOutputs().stream().map(transactionOutput -> transactionOutput.getDatumHash())
-                            .filter(dHash -> dHash != null)
-                            .collect(Collectors.toList());
-                    outputBlock.setDatumHashes(datumHash);
+                    if (showInputs) {
+                        // outputBlock.setShowInput(true);
+                        setInputs(inputs, outputBlock);
+                    }
+
+                    String formattedBlock = outputFormatter.formatBlock(outputBlock);
+                    System.out.println();
+                    System.out.println(formattedBlock);
+                    outputBlock.getInputs().clear();
                 }
 
-                outputBlock.setShowGrouping(true);
-                List<Amount> amounts = transactionBodies.stream()
-                        .flatMap(transactionBody ->
-                                transactionBody.getOutputs().stream()
-                                        .flatMap(txOutput -> txOutput.getAmounts().stream()))
-                        .collect(Collectors.toList());
-
-                List<Amount> mintAmounts = transactionBodies.stream()
-                        .flatMap(transactionBody -> transactionBody.getMint().stream())
-                        .collect(Collectors.toList());
-
-                setGroupAmount(amounts, outputBlock);
-
-                if (showMint) {
-                    //outputBlock.setShowMint(true);
-                    setMint(mintAmounts, outputBlock);
-                }
-
-                //Print Inputs
-                List<TransactionInput> inputs = transactionBodies.stream()
-                        .flatMap(transactionBody -> transactionBody.getInputs().stream())
-                        .collect(Collectors.toList());
-
-                if (showInputs) {
-                    // outputBlock.setShowInput(true);
-                    setInputs(inputs, outputBlock);
-                }
-
-                String formattedBlock = outputFormatter.formatBlock(outputBlock);
-                System.out.println();
-                System.out.println(formattedBlock);
-                outputBlock.getInputs().clear();
+                outputBlock.clear();
             }
-
-            outputBlock.clear();
         });
-
 
         Thread waitThread = startWaitThread();
         try {
@@ -203,10 +198,10 @@ public class BlockStreamerService {
             countDownLatch.await();
         } catch (InterruptedException ex) {
             waitThread.interrupt();
-            disposable.dispose();
+            blockSync.stop();
         } finally {
-            if (disposable != null)
-                disposable.dispose();
+            if (blockSync != null)
+                blockSync.stop();
         }
     }
 
