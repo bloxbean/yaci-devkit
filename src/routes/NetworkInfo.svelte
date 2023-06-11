36c815816ba5e2e90416fba440eed73f310f9de6
Satya <satran004@gmail.com>
2023-06-11 14:16:49 +0800

    Added null check and favicon

--- src/routes/NetworkInfo.svelte
+++ src/routes/NetworkInfo.svelte
@@ -17,7 +17,7 @@
             network_info.slot = currentMessage.slot;
             network_info.epoch_slot = currentMessage.epoch_slot;
             network_info.slots_per_epoch = currentMessage.slots_per_epoch;
-            if (network_info.epoch_slot &&  network_info.slots_per_epoch) {
+            if (network_info.epoch_slot && network_info.slots_per_epoch) {
                 network_info.epoch_progress = Math.round((network_info.epoch_slot / network_info.slots_per_epoch) * 100);
             }
         });
@@ -35,24 +35,28 @@
     <div class="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4">
         <div class="bg-gray-100 rounded-lg p-4">
             <h3 class="text-xl font-semibold mb-2">Epoch</h3>
-            <p class="text-gray-700">{network_info.epoch}</p>
+            <p class="text-gray-700">{network_info.epoch ? network_info.epoch : 0}</p>
         </div>
         <div class="bg-gray-100 rounded-lg p-4">
             <h3 class="text-xl font-semibold mb-2">Current Block</h3>
-            <p class="text-gray-700">{network_info.number}</p>
+            <p class="text-gray-700">{network_info.number ? network_info.number : 0}</p>
         </div>
         <div class="bg-gray-100 rounded-lg p-4">
             <h3 class="text-xl font-semibold mb-2">Slot</h3>
-            <p class="text-gray-700">{network_info.epoch_slot}/{network_info.slots_per_epoch}</p>
+            {#if network_info.epoch_slot}
+                <p class="text-gray-700">{network_info.epoch_slot}/{network_info.slots_per_epoch}</p>
+            {/if}
         </div>
         <div class="bg-gray-100 rounded-lg p-4">
             <h3 class="text-xl font-semibold mb-2">Epoch Progress</h3>
-            <div class="flex items-center">
-                <div class="w-3/4 bg-gray-300 rounded-lg overflow-hidden">
-                    <div class="bg-blue-500 h-2" style="width: {network_info.epoch_progress}%;"></div>
+            {#if network_info.epoch_progress}
+                <div class="flex items-center">
+                    <div class="w-3/4 bg-gray-300 rounded-lg overflow-hidden">
+                        <div class="bg-blue-500 h-2" style="width: {network_info.epoch_progress}%;"></div>
+                    </div>
+                    <p class="ml-2 text-gray-700">{network_info.epoch_progress}%</p>
                 </div>
-                <p class="ml-2 text-gray-700">{network_info.epoch_progress}%</p>
-            </div>
+            {/if}
         </div>
     </div>
 </section>
