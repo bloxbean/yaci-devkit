f66090eb813b2efb981648f2d713613fe7a50403
Satya <satran004@gmail.com>
2023-06-21 23:46:53 +0800

    Loading icon for live data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,23 @@
     import Blocks from "./blocks/Blocks.svelte";
     import Transactions from "./transactions/Transactions.svelte";
     import NetworkInfo from "./NetworkInfo.svelte";
+
+    import {onMount} from "svelte";
+
+    let blocks = [];
+    let unsubscribe;
+    onMount(async () => {
+        const store = await import('./blocks/store.js')
+
+        blocks = store.blocksCache;
+
+        unsubscribe = store.recentTxStore.subscribe(currentBlocks => {
+            if (blocks.length > 30)
+                blocks = [currentBlocks, ...blocks.slice(0, 30)];
+            else
+                blocks = [currentBlocks, ...blocks];
+        });
+    })
 </script>
 
 <section class="bg-gradient-to-r from-gray-900 to-black text-white py-20">
@@ -11,18 +28,24 @@
     </div>
 </section>
 
-<NetworkInfo></NetworkInfo>
+{#if blocks.length > 0 && Object.keys(blocks[0]).length === 0}
+    <section class="flex flex-col items-center mt-24">
+        <span class="text-xl font-bold mb-2 text-gray-800  px-4 py-2 rounded-lg">Waiting for live data</span>
+        <span class="loading loading-ring loading-lg"></span>
+    </section>
+{:else}
+    <NetworkInfo></NetworkInfo>
 
-
-<!-- Content Section -->
-<section class="container mx-auto py-10">
-    <div class="flex flex-col md:flex-row md:space-x-8">
-        <div class="w-full md:w-1/2">
-            <!-- Recent Blocks -->
-            <Blocks></Blocks>
+    <!-- Content Section -->
+    <section class="container mx-auto py-10">
+        <div class="flex flex-col md:flex-row md:space-x-8">
+            <div class="w-full md:w-1/2">
+                <!-- Recent Blocks -->
+                <Blocks></Blocks>
+            </div>
+            <div class="w-full md:w-1/2 mt-4 md:mt-0">
+                <Transactions></Transactions>
+            </div>
         </div>
-        <div class="w-full md:w-1/2 mt-4 md:mt-0">
-            <Transactions></Transactions>
-        </div>
-    </div>
-</section>
+    </section>
+{/if}
