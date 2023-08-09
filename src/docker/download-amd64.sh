6cc222b8e082d64fd5766e259ec5e28afc6d880c
Satya <satran004@gmail.com>
2023-08-09 14:01:57 +0800

    Updated node(8.1.2), yaci-store(0.0.11-beta3)

--- src/docker/download-amd64.sh
+++ src/docker/download-amd64.sh
@@ -1,5 +1,5 @@
-file=cardano-node-1.35.5-linux.tar.gz
-wget https://update-cardano-mainnet.iohk.io/cardano-node-releases/cardano-node-1.35.5-linux.tar.gz
+file=cardano-node-8.1.2-linux.tar.gz
+wget https://github.com/input-output-hk/cardano-node/releases/download/8.1.2/cardano-node-8.1.2-linux.tar.gz
 
 mkdir /app/cardano-bin
 
--- src/docker/download-arm64.sh
+++ src/docker/download-arm64.sh
@@ -1,4 +1,4 @@
-file=1_35_5.zip
+file=8_1_2.zip
 wget https://github.com/armada-alliance/cardano-node-binaries/raw/main/static-binaries/$file
 
 unzip $file
