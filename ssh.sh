7eff7e3e4f6d0c07b3c3f972815dce3e2acc05d7
Satya <satran004@gmail.com>
2023-08-20 21:54:48 +0800

    chore: ssh script

---
+++ ssh.sh
@@ -0,0 +1,9 @@
+CMD="docker-compose"
+if ! command -v docker-compose &> /dev/null
+then
+    echo "docker-compose not found, let's try 'docker compose'"
+    CMD="docker compose"
+fi
+
+$CMD --env-file env  exec yaci-cli /bin/bash
+
