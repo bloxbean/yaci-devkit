73cca7b70d442c9b88e3cc52b11169177dcdc553
Satya <satran004@gmail.com>
2024-05-03 20:53:32 +0800

    chore: Update to node 8.9.2 amd64 package

--- applications/cli/docker/download-amd64.sh
+++ applications/cli/docker/download-amd64.sh
@@ -1,5 +1,5 @@
-file=cardano-node-8.7.3-linux.tar.gz
-wget https://github.com/IntersectMBO/cardano-node/releases/download/8.7.3/cardano-node-8.7.3-linux.tar.gz
+file=cardano-node-8.9.2-linux.tar.gz
+wget https://github.com/IntersectMBO/cardano-node/releases/download/8.9.2/cardano-node-8.9.2-linux.tar.gz
 
 mkdir /app/cardano-bin
 
