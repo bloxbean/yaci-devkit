090656db2fa6e31844062916ecb94056e0577ea5
Satya <satran004@gmail.com>
2024-11-12 17:45:01 +0800

    Update Ogmios and Yaci versions; refine build script.
    Bump Ogmios to v6.9.0 and Yaci store to v0.1.1-graalvm-preview1 in the configuration files. Enhance the GitHub action build script by including lowercase architecture handling, ensuring artifact naming consistency. Enabled native mode for Yaci store by default.

--- applications/cli/docker/download-ogmios.sh
+++ applications/cli/docker/download-ogmios.sh
@@ -15,7 +15,7 @@ case $1 in
 esac
 
 
-version=v6.8.0
+version=v6.9.0
 file=ogmios-${version}-${SUFFIX}-linux.zip
 wget https://github.com/CardanoSolutions/ogmios/releases/download/${version}/$file
 
